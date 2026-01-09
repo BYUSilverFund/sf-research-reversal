f4447c1cbb93305a43a583d437586370c112bff8
brandonwaits12 <brandonwaits12@gmail.com>
2026-01-09 11:41:15 -0700

    data processing functions added

---
+++ research/experiments/brandon_test.py
@@ -0,0 +1,26 @@
+from research.utils.data_processing import get_barra_data, filter_data
+import datetime as dt
+
+if __name__ == "__main__":
+    print("hello world")
+    start = dt.date(1996, 1, 1)
+    end = dt.date(2024, 12, 31)
+    russell = True
+
+    columns = [
+        'date',
+        'barrid',
+        'ticker',
+        'price',
+        'return',
+        'market_cap', 
+        'bid_ask_spread',
+        'daily_volume',
+        'specific_return',
+        'specific_risk',
+        'yield'
+    ]
+
+    data = get_barra_data(start, end, columns, russell)
+    filtered_data = filter_data(data)
+    print(filtered_data)
--- research/utils/data_processing.py
+++ research/utils/data_processing.py
@@ -16,72 +16,39 @@ def get_barra_data(start: dt.date, end: dt.date, columns: list[str], russell: bo
     # convert returns to decimal space
     data = (
         data.with_columns(
-            pl.col('return').truediv(100).alias('return')))
-
-    data = (
-        data.with_columns(
-            pl.col('specific_return').truediv(100).alias('specific_return')))
-
-    data = (
-        data.with_columns(
-            pl.col('specific_risk').truediv(100).alias('specific_risk')
+            pl.col('return').truediv(100).alias('return'), 
+            pl.col('specific_return').truediv(100).alias('specific_return'), 
+            pl.col('specific_risk').truediv(100).alias('specific_risk'),
         )
     )
 
-    data = (
-        data.with_columns(
-            pl.col("daily_volume").replace(0, None)
-        )
+    # filter out invalid volume entries 
+    data = data.filter(
+        pl.col("daily_volume").is_not_null() &
+        (pl.col("daily_volume") > 1)
     )
-
-    if not russell:
-        # filter out securities we dont want to consider if we are using stocks outside of Russell 3000
-        data = data.filter(pl.col('iso_country_code').eq("USA"), pl.col('rootid').eq(pl.col('barrid')), pl.col('barrid').str.starts_with('US')) #pl.col('price').ge(5))
 
     return data
 
 
-def filter_prices(data: pl.DataFrame, signal: pl.DataFrame, lag: bool, price_filter: pl.Int64 | None = None, market_cap_filter: pl.Int64 | None = None) -> pl.DataFrame:
+def filter_data(data: pl.DataFrame, signal: pl.DataFrame, price_filter: pl.Int64 | None = None) -> pl.DataFrame:
 
     # filter out securities which will cause errors in portfolio calculuations such as null/NaN values
     # filter out low-price securities (penny stocks) as they are often non tradeable or skew results. 
 
+    # calculate lagged price
     data = data.sort(["barrid", "date"]).with_columns(
             pl.col("price")
             .shift(1)
             .over("barrid")
             .alias("price_lag")
             )
-    
-    data = data.sort(["barrid", "date"]).with_columns(
-            pl.col("market_cap")
-            .shift(1)
-            .over("barrid")
-            .alias("market_cap_lag")
-            )
-    
 
     # filter out no signal data
     data = data.filter(pl.col(signal).is_not_null(), pl.col(signal).is_not_nan())
 
-    # filter out no alpha data if alpha column exists
-    if 'alpha' in data.columns:
-        data = data.filter(
-            pl.col('alpha').is_not_null(), 
-            pl.col('alpha').is_not_nan()
-        )
-
     # filter based on parameters
-    if lag:
-        if price_filter:
-            data = data.filter(pl.col("price_lag") >= price_filter)
-        if market_cap_filter:
-            data = data.filter(pl.col("market_cap_lag") >= market_cap_filter)
-
-    else:
-        if price_filter:
-            data = data.filter(pl.col("price") >= price_filter)
-        if market_cap_filter:
-            data = data.filter(pl.col("market_cap") >= market_cap_filter)
+    if price_filter:
+        data = data.filter(pl.col("price_lag") >= price_filter)
     
     return data
