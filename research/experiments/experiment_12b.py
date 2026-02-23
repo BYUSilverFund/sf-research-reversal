ab55aff6350d0b624302ae3341b4a18e9174c6d0
brandonwaits12 <brandonwaits12@gmail.com>
2026-02-23 10:35:34 -0700

    added drawdown

--- research/experiments/experiment_12b.py
+++ research/experiments/experiment_12b.py
@@ -1,14 +1,12 @@
 import datetime as dt
 from pathlib import Path
 
-import altair as alt
-import great_tables as gt
 import polars as pl
-import sf_quant.data as sfd
-import statsmodels.formula.api as smf
+import sf_quant.performance as sfp
 
 # Parameters
-start = dt.date(1996, 1, 1)
+# start = dt.date(1996, 1, 1)
+start = dt.date(2021, 1, 1)
 end = dt.date(2024, 12, 31)
 signal_name = "barra_reversal_volume_clipped"
 gamma = 150
@@ -21,105 +19,115 @@ results_folder.mkdir(parents=True, exist_ok=True)
 weights = pl.read_parquet(f"weights/{signal_name}/{gamma}/*.parquet")
 
 # Get returns
-returns = (
-    sfd.load_assets(
-        start=start, end=end, columns=["date", "barrid", "return"], in_universe=True
-    )
-    .sort("date", "barrid")
-    .select(
-        "date",
-        "barrid",
-        pl.col("return").truediv(100).shift(-1).over("barrid").alias("forward_return"),
-    )
+# returns = (
+#     sfd.load_assets(
+#         start=start, end=end, columns=["date", "barrid", "return"], in_universe=True
+#     )
+#     .sort("date", "barrid")
+#     .select(
+#         "date",
+#         "barrid",
+#         pl.col("return").truediv(100).shift(-1).over("barrid").alias("forward_return"),
+#     )
+# )
+
+returns = sfp.generate_returns_from_weights(weights)
+drawdowns = sfp.generate_drawdown_from_returns(returns)
+
+drawdown_chart_path = results_folder / "drawdown_chart.png"
+sfp.generate_drawdown_chart(
+    drawdowns=drawdowns,
+    title="Volume-Adjusted Barra Reversal Drawdown",
+    file_name=drawdown_chart_path,
 )
 
-# Compute portfolio returns
-portfolio_returns = (
-    weights.join(other=returns, on=["date", "barrid"], how="left")
-    .group_by("date")
-    .agg(pl.col("forward_return").mul(pl.col("weight")).sum().alias("return"))
-    .sort("date")
-)
+# # Compute portfolio returns
+# portfolio_returns = (
+#     weights.join(other=returns, on=["date", "barrid"], how="left")
+#     .group_by("date")
+#     .agg(pl.col("forward_return").mul(pl.col("weight")).sum().alias("return"))
+#     .sort("date")
+# )
 
-# Compute cumulative log returns
-cumulative_returns = portfolio_returns.select(
-    "date", pl.col("return").log1p().cum_sum().mul(100).alias("cumulative_return")
-)
+# # Compute cumulative log returns
+# cumulative_returns = portfolio_returns.select(
+#     "date", pl.col("return").log1p().cum_sum().mul(100).alias("cumulative_return")
+# )
 
-# Plot cumulative log returns
-chart = (
-    alt.Chart(cumulative_returns, title="MVO Backtest Results (Active)")
-    .mark_line()
-    .encode(
-        x=alt.X("date", title=""),
-        y=alt.Y("cumulative_return", title="Cumulative Log Return (%)"),
-    )
-    .properties(width=800, height=400)
-)
+# # Plot cumulative log returns
+# chart = (
+#     alt.Chart(cumulative_returns, title="MVO Backtest Results (Active)")
+#     .mark_line()
+#     .encode(
+#         x=alt.X("date", title=""),
+#         y=alt.Y("cumulative_return", title="Cumulative Log Return (%)"),
+#     )
+#     .properties(width=800, height=400)
+# )
 
-# Save chart
-chart_path = results_folder / "cumulative_returns.png"
-chart.save(chart_path, scale_factor=3)
+# # Save chart
+# chart_path = results_folder / "cumulative_returns.png"
+# chart.save(chart_path, scale_factor=3)
 
-# Create summary table
-summary = portfolio_returns.select(
-    pl.col("return").mean().mul(252).alias("mean_return"),
-    pl.col("return").std().mul(pl.lit(252).sqrt()).alias("volatility"),
-).with_columns(pl.col("mean_return").truediv(pl.col("volatility")).alias("sharpe"))
+# # Create summary table
+# summary = portfolio_returns.select(
+#     pl.col("return").mean().mul(252).alias("mean_return"),
+#     pl.col("return").std().mul(pl.lit(252).sqrt()).alias("volatility"),
+# ).with_columns(pl.col("mean_return").truediv(pl.col("volatility")).alias("sharpe"))
 
-table = (
-    gt.GT(summary)
-    .tab_header(title="MVO Backtest Results (Active)")
-    .cols_label(
-        mean_return="Mean Return",
-        volatility="Volatility",
-        sharpe="Sharpe",
-    )
-    .fmt_percent(["mean_return", "volatility"], decimals=2)
-    .fmt_number("sharpe", decimals=2)
-    .opt_stylize(style=4, color="gray")
-)
+# table = (
+#     gt.GT(summary)
+#     .tab_header(title="MVO Backtest Results (Active)")
+#     .cols_label(
+#         mean_return="Mean Return",
+#         volatility="Volatility",
+#         sharpe="Sharpe",
+#     )
+#     .fmt_percent(["mean_return", "volatility"], decimals=2)
+#     .fmt_number("sharpe", decimals=2)
+#     .opt_stylize(style=4, color="gray")
+# )
 
-table_path = results_folder / "summary_table.png"
-table.save(table_path, scale=3)
+# table_path = results_folder / "summary_table.png"
+# table.save(table_path, scale=3)
 
-# Fama french regression
-ff5 = (
-    sfd.load_fama_french(start=start, end=end)
-    .sort("date")
-    .with_columns(pl.exclude("date").shift(-1))
-)
+# # Fama french regression
+# ff5 = (
+#     sfd.load_fama_french(start=start, end=end)
+#     .sort("date")
+#     .with_columns(pl.exclude("date").shift(-1))
+# )
 
-regression_data = (
-    portfolio_returns.join(other=ff5, on="date", how="left")
-    .drop_nulls("return")
-    .with_columns(pl.col("return").sub("rf").alias("return_rf"))
-    .with_columns(pl.exclude("date").mul(100))
-)
+# regression_data = (
+#     portfolio_returns.join(other=ff5, on="date", how="left")
+#     .drop_nulls("return")
+#     .with_columns(pl.col("return").sub("rf").alias("return_rf"))
+#     .with_columns(pl.exclude("date").mul(100))
+# )
 
-formula = "return_rf ~ mkt_rf + smb + hml + rmw + cma"
-model = smf.ols(formula, regression_data)
-results = model.fit()
+# formula = "return_rf ~ mkt_rf + smb + hml + rmw + cma"
+# model = smf.ols(formula, regression_data)
+# results = model.fit()
 
-regression_summary = pl.DataFrame(
-    {
-        "variable": results.params.index,
-        "coefficient": results.params.values,
-        "tstat": results.tvalues.values,
-    }
-)
+# regression_summary = pl.DataFrame(
+#     {
+#         "variable": results.params.index,
+#         "coefficient": results.params.values,
+#         "tstat": results.tvalues.values,
+#     }
+# )
 
-regression_table = (
-    gt.GT(regression_summary)
-    .tab_header(title="MVO Backtest Results (Active) (Daily %)")
-    .cols_label(
-        variable="Variable",
-        coefficient="Coefficient",
-        tstat="T-stat",
-    )
-    .fmt_number(["coefficient", "tstat"], decimals=4)
-    .opt_stylize(style=4, color="gray")
-)
+# regression_table = (
+#     gt.GT(regression_summary)
+#     .tab_header(title="MVO Backtest Results (Active) (Daily %)")
+#     .cols_label(
+#         variable="Variable",
+#         coefficient="Coefficient",
+#         tstat="T-stat",
+#     )
+#     .fmt_number(["coefficient", "tstat"], decimals=4)
+#     .opt_stylize(style=4, color="gray")
+# )
 
-table_path = results_folder / "regression_table.png"
-regression_table.save(table_path, scale=3)
+# table_path = results_folder / "regression_table.png"
+# regression_table.save(table_path, scale=3)
